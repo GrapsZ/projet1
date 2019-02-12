7141808581090dd6ef06fd925dc75e0a047e4023
Michael THAIZE <33883629+GrapsZ@users.noreply.github.com>
2019-02-12 16:46:55 +0100

    Fix : fermeture des balises
    Les balises n'étaient pas fermées suite à un oubli de commentaire non fermé de ma part.

--- inc/footer.php
+++ inc/footer.php
@@ -9,7 +9,6 @@
     </script>
     <script src="js/bootstrap.bundle.min.js" crossorigin="anonymous"
     ></script>
-     <!--<iframe width='560' height='315' src='https://www.youtube.com/embed/bnVUHWCynig' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>
 </body>
 </html>
 <?php
